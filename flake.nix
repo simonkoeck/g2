2be066825decd38116cb2b6b427a64c37e2a32ac
Simon Koeck <simon@koeck.dev>
2026-02-04 15:11:52 +0100

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "G2 - Smart Git merge with semantic conflict resolution";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "g2";
+            version = "0.1.0";
+
+            src = ./.;
+
+            # Hash for Go module dependencies
+            vendorHash = "sha256-xcrVZigGUJJYz0JDMEYagoaDDG4l1GwBbtbvOE01oY4=";
+
+            meta = with pkgs.lib; {
+              description = "Smart Git merge with semantic conflict resolution";
+              homepage = "https://github.com/simonkoeck/g2";
+              license = licenses.mit;
+              maintainers = [ ];
+              mainProgram = "g2";
+            };
+          };
+
+          g2 = self.packages.${system}.default;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.default;
+          };
+          g2 = self.apps.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            git
+          ];
+        };
+      }
+    );
+}
